e20c7ce9bbcde6b92d9e99c7c0a186525893f326
GeekyGeeky <therealolushola@gmail.com>
2025-10-16 15:01:05 +0100

    update server * added cors and env plugin * register env schema * update port and host to use env schema

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,32 @@
 import fastify from 'fastify';
-
+import cors from '@fastify/cors'
+import env from '@fastify/env'
+import { schema } from './config/env.js'
 
 const server = fastify({
-  logger: true,
+    logger: true,
+});
+
+await server.register(env, {
+    dotenv: true,
+    data: process.env,
+    schema,
+});
+
+await server.register(cors, {
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
 export const startServer = async () => {
-  try {
-    const address = await server.listen({
-      port: 8080,
-      host: '0.0.0.0',
-    });
-    server.log.info(`Server running at ${address}`);
-  } catch (error) {
-    server.log.error(error);
-    process.exit(1);
-  }
+    try {
+        const address = await server.listen({
+            port: server.config.PORT,
+            host: server.config.HOST,
+        });
+        server.log.info(`Server running at ${address}`);
+    } catch (error) {
+        server.log.error(error);
+        process.exit(1);
+    }
 };
